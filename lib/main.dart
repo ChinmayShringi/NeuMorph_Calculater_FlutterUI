a30322dd88deadf465e226e8cf9e7ad51b3a45e0
ChinmayShringi <chinmayshringi4@gmail.com>
2021-05-26 16:20:53 +0530

    add result

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      home: CalculatorNeuApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+const Color colorDark = Color(0xFF374352);
+const Color colorLight = Color(0xFFe6eeff);
+
+class CalculatorNeuApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _CalculatorNeuAppState createState() => _CalculatorNeuAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CalculatorNeuAppState extends State<CalculatorNeuApp> {
+  bool dMode = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculater'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      backgroundColor: dMode ? colorDark : colorLight,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          dMode ? dMode = false : dMode = true;
+                        });
+                      },
+                      child: _switchMode(),
+                    ),
+                    SizedBox(height: 80),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        '1',
+                        style: TextStyle(
+                            fontSize: 55,
+                            fontWeight: FontWeight.bold,
+                            color: dMode ? Colors.white : Colors.red),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '=',
+                          style: TextStyle(
+                              fontSize: 35,
+                              color: dMode ? Colors.green : Colors.grey),
+                        ),
+                        Text(
+                          '1+2',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: dMode ? Colors.green : Colors.grey),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
