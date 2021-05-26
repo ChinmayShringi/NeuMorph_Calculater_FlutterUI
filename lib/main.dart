5cb5c378ffe909c389bd395a1c7df60563468274
ChinmayShringi <chinmayshringi4@gmail.com>
2021-05-26 16:21:55 +0530

    add neumorph

--- lib/main.dart
+++ lib/main.dart
@@ -85,9 +85,222 @@ class _CalculatorNeuAppState extends State<CalculatorNeuApp> {
                   ],
                 ),
               ),
+              Container(
+                child: Column(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buttonTopSet(title: 'sin'),
+                      _buttonTopSet(title: 'cos'),
+                      _buttonTopSet(title: 'tan'),
+                      _buttonTopSet(title: '%')
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildButtonRounded(
+                          title: 'C',
+                          textColor: dMode ? Colors.green : Colors.redAccent),
+                      _buildButtonRounded(title: '('),
+                      _buildButtonRounded(title: ')'),
+                      _buildButtonRounded(
+                          title: '/',
+                          textColor: dMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildButtonRounded(title: '7'),
+                      _buildButtonRounded(title: '8'),
+                      _buildButtonRounded(title: '9'),
+                      _buildButtonRounded(
+                          title: 'x',
+                          textColor: dMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildButtonRounded(title: '4'),
+                      _buildButtonRounded(title: '5'),
+                      _buildButtonRounded(title: '6'),
+                      _buildButtonRounded(
+                          title: '-',
+                          textColor: dMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildButtonRounded(title: '1'),
+                      _buildButtonRounded(title: '2'),
+                      _buildButtonRounded(title: '3'),
+                      _buildButtonRounded(
+                          title: '+',
+                          textColor: dMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildButtonRounded(title: '0'),
+                      _buildButtonRounded(title: ','),
+                      _buildButtonRounded(
+                          icon: Icons.backspace_outlined,
+                          iconColor: dMode ? Colors.green : Colors.redAccent),
+                      _buildButtonRounded(
+                          title: '=',
+                          textColor: dMode ? Colors.green : Colors.redAccent)
+                    ],
+                  )
+                ]),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildButtonRounded(
+      {String title,
+      double padding = 17,
+      IconData icon,
+      Color iconColor,
+      Color textColor}) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: NeuContainer(
+        dMode: dMode,
+        borderRadius: BorderRadius.circular(40),
+        padding: EdgeInsets.all(padding),
+        child: Container(
+          width: padding * 2,
+          height: padding * 2,
+          child: Center(
+              child: title != null
+                  ? Text(
+                      '$title',
+                      style: TextStyle(
+                          color: textColor != null
+                              ? textColor
+                              : dMode
+                                  ? Colors.white
+                                  : Colors.black,
+                          fontSize: 30),
+                    )
+                  : Icon(
+                      icon,
+                      color: iconColor,
+                      size: 30,
+                    )),
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonTopSet({String title, double padding = 17}) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: NeuContainer(
+        dMode: dMode,
+        borderRadius: BorderRadius.circular(50),
+        padding:
+            EdgeInsets.symmetric(horizontal: padding, vertical: padding / 2),
+        child: Container(
+          width: padding * 2,
+          child: Center(
+            child: Text(
+              '$title',
+              style: TextStyle(
+                  color: dMode ? Colors.white : Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _switchMode() {
+    return NeuContainer(
+      dMode: dMode,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      borderRadius: BorderRadius.circular(40),
+      child: Container(
+        width: 70,
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.wb_sunny,
+            color: dMode ? Colors.grey : Colors.redAccent,
+          ),
+          Icon(
+            Icons.nightlight_round,
+            color: dMode ? Colors.green : Colors.grey,
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class NeuContainer extends StatefulWidget {
+  final bool dMode;
+  final Widget child;
+  final BorderRadius borderRadius;
+  final EdgeInsetsGeometry padding;
+  NeuContainer(
+      {this.dMode = false, this.child, this.borderRadius, this.padding});
+  @override
+  _NeuContainerState createState() => _NeuContainerState();
+}
+
+class _NeuContainerState extends State<NeuContainer> {
+  bool _isPressed = false;
+
+  void _onPointerDown(PointerDownEvent event) {
+    setState(() {
+      _isPressed = true;
+    });
+  }
+
+  void _onPointerUp(PointerUpEvent event) {
+    setState(() {
+      _isPressed = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool dMode = widget.dMode;
+    return Listener(
+      onPointerDown: _onPointerDown,
+      onPointerUp: _onPointerUp,
+      child: Container(
+        padding: widget.padding,
+        decoration: BoxDecoration(
+            color: dMode ? colorDark : colorLight,
+            borderRadius: widget.borderRadius,
+            boxShadow: _isPressed
+                ? null
+                : [
+                    BoxShadow(
+                      color: dMode ? Colors.black54 : Colors.blueGrey.shade200,
+                      offset: Offset(4.0, 4.0),
+                      blurRadius: 15.0,
+                      spreadRadius: 1.0,
+                    ),
+                    BoxShadow(
+                        color: dMode ? Colors.blueGrey.shade700 : Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0)
+                  ]),
+        child: widget.child,
       ),
     );
   }
